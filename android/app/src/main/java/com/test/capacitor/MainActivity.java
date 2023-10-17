8328b0e9e0a71e193294bbd4cc08b65dee04ce20
Vyacheslav Kovtun <kovtyn11191@gmail.com>
2023-10-17 15:17:20 +0300

    add pop up function call

--- android/app/src/main/java/com/test/capacitor/MainActivity.java
+++ android/app/src/main/java/com/test/capacitor/MainActivity.java
@@ -1,5 +1,15 @@
 package com.test.capacitor;
 
+import com.test.plugins.popup.PopupPluginPlugin;
+
+import android.os.Bundle;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    registerPlugin(PopupPluginPlugin.class);
+    super.onCreate(savedInstanceState);
+  }
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PopupPlugin } from 'popup-plugin';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-capacitor';
+
+  async showPopup() {
+    await PopupPlugin.showPopup({message: 'Hello World'});
+  }
 }
